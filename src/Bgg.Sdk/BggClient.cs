f70d23d1a79d53e6fcc8db23556f50324a5bdb82
Tyler Hundley <theiam79@gmail.com>
2021-12-09 00:12:32 -0600

    Add comments for bggclient

--- src/Bgg.Sdk/BggClient.cs
+++ src/Bgg.Sdk/BggClient.cs
@@ -8,32 +8,61 @@ using System.Threading.Tasks;
 
 namespace Bgg.Sdk
 {
+    /// <summary>
+    /// The default implementation of <see cref="IBggClient"/>
+    /// </summary>
     public class BggClient : IBggClient
     {
         private readonly IBggApi _bggApi;
         private readonly IMapper _mapper;
 
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        /// <param name="bggApi">The <see cref="IBggApi"/> used to make requests</param>
+        /// <param name="mapper">The <see cref="IMapper"/> used to map responses</param>
         public BggClient(IBggApi bggApi, IMapper mapper)
         {
             _bggApi = bggApi;
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.Collection> CollectionAsync(string username) => CollectionAsync(new Core.Collection.QueryParameters(username));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.Collection> CollectionAsync(Core.Collection.QueryParameters queryParameters)
         {
             var response = await _bggApi.GetCollection(queryParameters);
             return _mapper.Map<Models.Collection>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.FamilyCollection> FamilyAsync(int id) => FamilyAsync(new Core.Family.QueryParameters(id));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.FamilyCollection> FamilyAsync(Core.Family.QueryParameters queryParameters)
         {
             var response = await _bggApi.Family(queryParameters);
             return _mapper.Map<Models.FamilyCollection>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.Forum> ForumAsync(int id) => ForumAsync(new Core.Forum.QueryParameters(id));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.Forum> ForumAsync(Core.Forum.QueryParameters queryParameters)
         {
             var response = await _bggApi.Forum(queryParameters);
@@ -41,57 +70,122 @@ namespace Bgg.Sdk
             return _mapper.Map<Models.Forum>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.ForumList> ForumListAsync(int id) => ForumListAsync(new Core.ForumList.QueryParameters(id));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.ForumList> ForumListAsync(Core.ForumList.QueryParameters queryParameters)
         {
             var response = await _bggApi.ForumList(queryParameters);
             return _mapper.Map<Models.ForumList>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.Guild> GuildAsync(int id) => GuildAsync(new Core.Guild.QueryParameters(id));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.Guild> GuildAsync(Core.Guild.QueryParameters queryParameters)
         {
             var response = await _bggApi.Guild(queryParameters);
             return _mapper.Map<Models.Guild>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.HotList> HotListAsync(Core.HotItems.HotListType hotListType) => HotListAsync(new Core.HotItems.QueryParameters(hotListType));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.HotList> HotListAsync(Core.HotItems.QueryParameters queryParameters)
         {
             var response = await _bggApi.HotItems(queryParameters);
             return _mapper.Map<Models.HotList>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.PlayInfo> PlayInfoAsync(string username) => PlayInfoAsync(new Core.Plays.QueryParameters(username));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.PlayInfo> PlayInfoAsync(int id) => PlayInfoAsync(new Core.Plays.QueryParameters(id));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.PlayInfo> PlayInfoAsync(string username, int id) => PlayInfoAsync(new Core.Plays.QueryParameters(username, id));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.PlayInfo> PlayInfoAsync(Core.Plays.QueryParameters queryParameters)
         {
             var response = await _bggApi.Plays(queryParameters);
             return _mapper.Map<Models.PlayInfo>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.SearchResult> SearchAsync(string searchTerm) => SearchAsync(new Core.Search.QueryParameters(searchTerm));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>`
         public async Task<Models.SearchResult> SearchAsync(Core.Search.QueryParameters queryParameters)
         {
             var response = await _bggApi.Search(queryParameters);
             return _mapper.Map<Models.SearchResult>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.ThingCollection> ThingAsync(int id) => ThingAsync(new Core.Thing.QueryParameters(id));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.ThingCollection> ThingAsync(Core.Thing.QueryParameters queryParameters)
         {
             var response = await _bggApi.Things(queryParameters);
             return _mapper.Map<Models.ThingCollection>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public Task<Models.Thread> ThreadAsync(int id) => ThreadAsync(new Core.Thread.QueryParameters(id));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.Thread> ThreadAsync(Core.Thread.QueryParameters queryParameters)
         {
             var response = await _bggApi.Thread(queryParameters);
             return _mapper.Map<Models.Thread>(response);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Task<Models.User> UserAsync(string username) => UserAsync(new Core.User.QueryParameters(username));
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public async Task<Models.User> UserAsync(Core.User.QueryParameters queryParameters)
         {
             var response = await _bggApi.User(queryParameters);
--- src/Bgg.Sdk/IBggClient.cs
+++ src/Bgg.Sdk/IBggClient.cs
@@ -3,30 +3,155 @@ using Bgg.Sdk.Models;
 
 namespace Bgg.Sdk
 {
+    /// <summary>
+    /// A client to handle making requests to the BGG XMLAPI2 and map the responses
+    /// </summary>
     public interface IBggClient
     {
+        /// <summary>
+        /// Get the collection for a given user
+        /// </summary>
+        /// <param name="queryParameters">The options for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the collection information</returns>
         Task<Collection> CollectionAsync(Core.Collection.QueryParameters queryParameters);
+        /// <summary>
+        /// Get the collection for the specificed <paramref name="username"/> using the default query parameters.
+        /// </summary>
+        /// <param name="username">The username to retrieve collection info for</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the collection information</returns>
         Task<Collection> CollectionAsync(string username);
+        /// <summary>
+        /// Get the information for a single family using the default query parameters.
+        /// </summary>
+        /// <param name="id">The id of the family</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the family information</returns>
         Task<FamilyCollection> FamilyAsync(int id);
+        /// <summary>
+        /// Get information for the specified families
+        /// </summary>
+        /// <param name="queryParameters">The options for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the family information</returns>
         Task<FamilyCollection> FamilyAsync(Core.Family.QueryParameters queryParameters);
+        /// <summary>
+        /// Get information for the specified forum using the default query parameters.
+        /// </summary>
+        /// <param name="id">The id of the forum</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the forum information</returns>
         Task<Forum> ForumAsync(int id);
+        /// <summary>
+        /// Get information for the specified forum.
+        /// </summary>
+        /// <param name="queryParameters">The options for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the forum information</returns>
         Task<Forum> ForumAsync(Core.Forum.QueryParameters queryParameters);
+        /// <summary>
+        /// Get a list of forums for a specific object using the default query parameters.
+        /// </summary>
+        /// <param name="id">The id of the object</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the forumlist</returns>
         Task<ForumList> ForumListAsync(int id);
+        /// <summary>
+        /// Get a list of forums for a specific object.
+        /// </summary>
+        /// <param name="queryParameters">The options for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the forumlist</returns>
         Task<ForumList> ForumListAsync(Core.ForumList.QueryParameters queryParameters);
+        /// <summary>
+        /// Get information for the specified guild using the default query parameters.
+        /// </summary>
+        /// <param name="id">The id of the guild</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the guild information</returns>
         Task<Guild> GuildAsync(int id);
+        /// <summary>
+        /// Get information for the specified guild.
+        /// </summary>
+        /// <param name="queryParameters">The options for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the guild information</returns>
         Task<Guild> GuildAsync(Core.Guild.QueryParameters queryParameters);
+        /// <summary>
+        /// Get the hotlist for a givent type using the default query parameters.
+        /// </summary>
+        /// <param name="hotListType">The hotlist type</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the hotlist</returns>
         Task<HotList> HotListAsync(HotListType hotListType);
+        /// <summary>
+        /// Get the hotlist for a given type.
+        /// </summary>
+        /// <param name="queryParameters">The options for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the hotlist</returns>
         Task<HotList> HotListAsync(QueryParameters queryParameters);
+        /// <summary>
+        /// Gets play information for a specified object using the default query parameters.
+        /// </summary>
+        /// <param name="id">The object to retrieve play information for</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the play information</returns>
         Task<PlayInfo> PlayInfoAsync(int id);
+        /// <summary>
+        /// Gets play information for a specified user and/or object.
+        /// </summary>
+        /// <param name="queryParameters">The options to use for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the play information</returns>
         Task<PlayInfo> PlayInfoAsync(Core.Plays.QueryParameters queryParameters);
+        /// <summary>
+        /// Gets play information for a specified user using the default query parameters.
+        /// </summary>
+        /// <param name="username">The user to retrieve play information for</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the play information</returns>
         Task<PlayInfo> PlayInfoAsync(string username);
+        /// <summary>
+        /// Gets play information for a specified user and object using the default query parameters.
+        /// </summary>
+        /// <param name="username">The user to retrieve play information for</param>
+        /// <param name="id">The object to retrieve play information for</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the play information</returns>
         Task<PlayInfo> PlayInfoAsync(string username, int id);
+        /// <summary>
+        /// Search for items in the BGG database.
+        /// </summary>
+        /// <param name="queryParameters">The optiosn to use for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the search results</returns>
         Task<SearchResult> SearchAsync(Core.Search.QueryParameters queryParameters);
+        /// <summary>
+        /// Search for items in the BGG database using the default query parameters.
+        /// </summary>
+        /// <param name="searchTerm">The name to search for</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the search results</returns>
         Task<SearchResult> SearchAsync(string searchTerm);
+        /// <summary>
+        /// Get information for the specified thing using the default query parameters.
+        /// </summary>
+        /// <param name="id">The id of the thing</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the thing information</returns>
         Task<ThingCollection> ThingAsync(int id);
+        /// <summary>
+        /// Get information for the specified thing(s).
+        /// </summary>
+        /// <param name="queryParameters">The options to use for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the thing information</returns>
         Task<ThingCollection> ThingAsync(Core.Thing.QueryParameters queryParameters);
+        /// <summary>
+        /// Get information for the specified thread using the default query parameters.
+        /// </summary>
+        /// <param name="id">The id of the thread</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the thread information</returns>
         Task<Models.Thread> ThreadAsync(int id);
+        /// <summary>
+        /// Get information for the specified thread.
+        /// </summary>
+        /// <param name="queryParameters">The options to use for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the thread information</returns>
         Task<Models.Thread> ThreadAsync(Core.Thread.QueryParameters queryParameters);
+        /// <summary>
+        /// Get information for the specified user using the default query parameters.
+        /// </summary>
+        /// <param name="username">The username to retrieve information for</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the user information</returns>
+        Task<Models.User> UserAsync(string username);
+        /// <summary>
+        /// Get information for the specified user.
+        /// </summary>
+        /// <param name="queryParameters">The options to use for the request</param>
+        /// <returns>A <see cref="Task"/> representing the query. The task result contains the user information</returns>
         Task<User> UserAsync(Core.User.QueryParameters queryParameters);
     }
 }
